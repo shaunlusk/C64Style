1cbfc1f1eb78b6d0a3bf9a8a3ee36dfd1b43302c
Shaun Lusk <slusk@accruent.com>
2016-11-26 00:26:22 -0600

    gfxelement tests in progress

--- test/GfxElement.test.js
+++ test/GfxElement.test.js
@@ -1,4 +1,104 @@
 describe("GfxElement", function() {
+  var element;
+
+  before(function() {
+    element = getGfxElement();
+  });
+
+  describe("#getId()", function() {
+    it("should return element id", function() {
+      var id = element.getId();
+      assert(id);
+    });
+  });
+  describe("#isDirty()", function() {
+    it("should true if dirty", function() {
+      element.setDirty(true);
+      var dirty = element.isDirty();
+      assert(dirty, "should have been dirty");
+    });
+    it("should true if hasCollision", function() {
+      element.setHasCollision(true);
+      var dirty = element.isDirty();
+      assert(dirty, "should have been dirty");
+    });
+    it("should true if hadCollisionPreviousFrame", function() {
+      element._hadCollisionPreviousFrame = true;
+      var dirty = element.isDirty();
+      assert(dirty, "should have been dirty");
+    });
+  });
+  describe("#setDirty()", function() {
+    it("should set dirty", function() {
+      element.setDirty(true);
+      var dirty = element._dirty;
+      assert(dirty, "should have been dirty");
+    });
+  });
+  describe("#isHidden()", function() {
+    it("should return isHidden", function() {
+      element.setHidden(false);
+      var hidden = element.isHidden();
+      assert(!hidden, "should not have been hidden");
+    });
+  });
+  describe("#setHidden()", function() {
+    it("should return isHidden", function() {
+      element.setHidden(true);
+      var hidden = element.isHidden();
+      assert(hidden, "should have been hidden");
+    });
+  });
+  describe("#hasCollision()", function() {
+    it("should return hasCollision", function() {
+      element.setHasCollision(true);
+      var hasCollision = element.hasCollision();
+      assert(hasCollision, "should have had collision");
+    });
+  });
+  describe("#setHasCollision()", function() {
+    it("should set has collision", function() {
+      element.setHasCollision(true);
+      element.setHasCollision(false);
+      var hasCollision = element.hasCollision();
+      assert(!hasCollision, "should have had collision");
+    });
+  });
+  describe("#getZIndex()", function() {
+    it("should return zindex", function() {
+      var expected = 50;
+      element.setZIndex(expected);
+      var zindex = element.getZIndex();
+      assert(zindex, "should have returned zindex");
+    });
+  });
+  describe("#setZIndex()", function() {
+    it("should set zindex", function() {
+      var expected = 250;
+      element.setZIndex(50);
+      element.setZIndex(expected);
+      var zindex = element.getZIndex();
+      assert(zindex, "should have set zindex");
+    });
+  });
+  describe("#getParentLayer()", function() {
+    it("should return parentLayer", function() {
+      var layer = element.getParentLayer();
+      assert(layer);
+    });
+  });
+  describe("#getCanvasContext()", function() {
+    it("should return canvas context", function() {
+      var context = element.getCanvasContext();
+      assert(context);
+    });
+  });
+  describe("#getScreenContext()", function() {
+    it("should return screen context", function() {
+      var context = element.getScreenContext();
+      assert(context);
+    });
+  });
   describe("#collidesWithCoordinates()", function() {
     it("should return false if coord x < element x", function(done) {
       var element = getGfxElement();
