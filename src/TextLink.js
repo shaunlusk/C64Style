2055e48f13ce70c46df28cebf7ef8f806f675b60
Shaun Lusk <shaun.lusk@gmail.com>
2019-06-08 14:33:21 -0500

    TextLink

--- src/TextLink.js
+++ src/TextLink.js
@@ -1,12 +1,13 @@
-var C64Style = C64Style || {};
+import TextElement from './TextElement';
+import Utils from 'slcommon/src/Utils';
 
 /** TextElement that draws a text string or a text symbol to a canvas and provides a Hyperlink to another location.<br />
-* <b>Extends</b>: {@link C64Style.TextElement}
+* <b>Extends</b>: {@link TextElement}
 * @constructor
-* @param {C64Style.C64Screen} screenContext The target screen.
-* @param {C64Style.GfxLayer} parentLayer The parent layer that will draw this element.
+* @param {C64Screen} screenContext The target screen.
+* @param {GfxLayer} parentLayer The parent layer that will draw this element.
 * @param {Object} props Properties for this TextLink.  Supports:
-* From {@link C64Style.GfxElement}
+* From {@link GfxElement}
 *   <ul>
 *     <li>scaleX - integer - Horizontal scale of this element.  Independent of screen scale.</li>
 *     <li>scaleY - integer - Vertical scale of this element.  Independent of screen scale.</li>
@@ -18,12 +19,12 @@ var C64Style = C64Style || {};
 * From {@link TextElement}:
 * <ul>
 *   <li>text - string - The text for this element. A text string or a symbol name is required for TextElement.</li>
-*   <li>symbolName - string - The symbolName for this element.  A text string or a symbol name is required for TextElement.  Refer to symbol names in {@link C64Style.CharacterMap}.</li>
-*   <li>color - Color - The color for the text. Default: C64Style.Color.LIGHTBLUE</li>
+*   <li>symbolName - string - The symbolName for this element.  A text string or a symbol name is required for TextElement.  Refer to symbol names in {@link CharacterMap}.</li>
+*   <li>color - Color - The color for the text. Default: Color.LIGHTBLUE</li>
 *   <li>backgroundColor - Color - The backgroundColor for the text. Default: no background color</li>
 *   <li>characterRenderer - CharacterRenderer - The renderer to use to draw text.
 *     This can be shared with a renderer for drawing text elements.  If a renderer is not provided,
-*     This TextLayer will create a {@link C64Style.CharacterRenderer}.</li>
+*     This TextLayer will create a {@link CharacterRenderer}.</li>
 * </ul>
 * For TextLink:
 * <ul>
@@ -34,9 +35,9 @@ var C64Style = C64Style || {};
 *   <li>newWindow - boolean - Whether to open the new location in a new window or not.</li>
 * </ul>
 */
-C64Style.TextLink = function(screenContext, parentLayer, props) {
+function TextLink(props) {
   props = props || {};
-  C64Style.TextElement.call(this, screenContext, parentLayer, props);
+  TextElement.call(this, props);
   this._mouseOverColor = props.mouseOverColor || this._color;
   this._mouseOverBackgroundColor = props.mouseOverBackgroundColor || this._backgroundColor;
   this._onClick = props.onClick;
@@ -44,26 +45,28 @@ C64Style.TextLink = function(screenContext, parentLayer, props) {
   this._newWindow = props.newWindow;
   this._baseColor = this._color;
   this._baseBackgroundColor = this._backgroundColor;
+  this._windowOpen = props.windowOpen || window.open;
+  this._setDocumentLocation = props.setDocumentLocation || ((location) => {document.location = location});
   this.on("MOUSE_UP_ON_ELEMENT", this._click.bind(this));
   this.on("MOUSE_ENTER_ELEMENT", this._enter.bind(this));
   this.on("MOUSE_EXIT_ELEMENT", this._exit.bind(this));
 };
 
-C64Style.TextLink.prototype = new C64Style.TextElement(null, null, {characterRenderer:{}});
-C64Style.TextLink.prototype.constructor = C64Style.TextLink;
+TextLink.prototype = new TextElement({characterRenderer:{}});
+TextLink.prototype.constructor = TextLink;
 
 /** @private */
-C64Style.TextLink.prototype._enter = function() {
+TextLink.prototype._enter = function() {
   this._setActive(true);
 };
 
 /** @private */
-C64Style.TextLink.prototype._exit = function() {
+TextLink.prototype._exit = function() {
   this._setActive(false);
 };
 
 /** @private */
-C64Style.TextLink.prototype._setActive = function(bool) {
+TextLink.prototype._setActive = function(bool) {
   this._mouseIsOver = bool;
   if (bool) {
     this.setColor(this._mouseOverColor);
@@ -76,16 +79,18 @@ C64Style.TextLink.prototype._setActive = function(bool) {
 };
 
 /** @private */
-C64Style.TextLink.prototype._click = function(event) {
-  if (SL.isFunction(this._onClick)) {
+TextLink.prototype._click = function(event) {
+  if (Utils.isFunction(this._onClick)) {
     var result = this._onClick(event);
     if (result !== undefined && !result) return;
   }
   if (this._href) {
     if (this._newWindow) {
-      window.open(this._href, "_blank");
+      this._windowOpen(this._href, "_blank");
     } else {
-      document.location = this._href;
+      this._setDocumentLocation(this._href);
     }
   }
 };
+
+export default TextLink;
